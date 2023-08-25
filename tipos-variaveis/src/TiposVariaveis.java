72dd3837acf2cc0a1b05f8de7fce6b530d542e42
eduardocamilio <eduardocamilio137@gmail.com>
2023-08-24 23:36:20 -0300

    testing the diferents types of variables

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short ncurto = 1;
+        int niteiro = ncurto;
+        short ncurto2 = (short) niteiro;
+
+        int n = 1;
+
+        numero = 2;
+
+
     }
 }
