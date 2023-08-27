014e2b05a91cc1ff99dc33635e93692603c7236a
eduardocamilio <eduardocamilio137@gmail.com>
2023-08-27 17:29:31 -0300

    using the terminal

---
+++ sistema-smart-tv/src/AboutMe.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class AboutMe {
+    
+    public static void main(String[] args) {
+        //Obtendo dados do usuário
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        //Imprimindo dados que o usuario digitou.
+        System.out.println("Olá, meu nome é " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("Minha altura é " + altura + " cm ");
+    }
+}
