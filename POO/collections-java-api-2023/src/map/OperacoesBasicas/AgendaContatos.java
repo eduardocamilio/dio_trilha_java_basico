21918c33bfb25e51887c613ec6fed5b118ad9b55
eduardocamilio <eduardocamilio137@gmail.com>
2023-09-10 18:18:39 -0300

    Resolvendo exercicios

--- POO/collections-java-api-2023/src/map/OperacoesBasicas/AgendaContatos.java
+++ POO/collections-java-api-2023/src/map/OperacoesBasicas/AgendaContatos.java
@@ -48,17 +48,17 @@ public class AgendaContatos {
     AgendaContatos agendaContatos = new AgendaContatos();
 
     // Adicionar contatos
-    agendaContatos.adicionarContato("Camila", 123456);
+    agendaContatos.adicionarContato("Thayna", 123456);
     agendaContatos.adicionarContato("João", 5665);
     agendaContatos.adicionarContato("Carlos", 1111111);
     agendaContatos.adicionarContato("Ana", 654987);
-    agendaContatos.adicionarContato("Maria", 1111111);
+    agendaContatos.adicionarContato("Adriana", 1111111);
     agendaContatos.adicionarContato("Camila", 44444);
 
     agendaContatos.exibirContatos();
 
     // Remover um contato
-    agendaContatos.removerContato("Maria");
+    agendaContatos.removerContato("Ana");
     agendaContatos.exibirContatos();
 
     // Pesquisar número por nome
